969b4950f736f2270ca8112b88a0d7762f83c708
Aldik <andi.kitta77@gmail.com>
2022-08-28 18:12:08 +0800

    Room DB

--- app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/local/ToDoDatabase.kt
+++ app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/local/ToDoDatabase.kt
@@ -1,8 +1,14 @@
 package com.aldikitta.thingstodo.foundation.datasource.local
 
+import android.content.Context
 import androidx.room.*
-import com.aldikitta.thingstodo.foundation.datasource.local.model.ToDoGroupDb
-import com.aldikitta.thingstodo.foundation.datasource.local.model.ToDoStepDb
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.aldikitta.thingstodo.foundation.datasource.local.model.*
+import com.aldikitta.thingstodo.foundation.wrapper.DateTimeProviderImpl
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 @Database(
     entities = [
@@ -20,5 +26,54 @@ import com.aldikitta.thingstodo.foundation.datasource.local.model.ToDoStepDb
 
 @TypeConverters(DateConverter::class)
 abstract class ToDoDatabase : RoomDatabase() {
-    abstract fun todoWriteDao(): ToDoWriteDao
+    abstract fun toDoWriteDao(): ToDoWriteDao
+    abstract fun todoReadDao(): ToDoReadDao
+
+    @DelicateCoroutinesApi
+    companion object {
+        private const val TODO_DB_NAME = "todo-db"
+
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getInstance(context: Context): ToDoDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): ToDoDatabase {
+            val db = Room.databaseBuilder(
+                context,
+                ToDoDatabase::class.java,
+                TODO_DB_NAME
+            )
+                .addCallback(
+                    object : Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+
+                            GlobalScope.launch(Dispatchers.IO) {
+                                initPrePopulateDefaultGroup(context)
+                            }
+                        }
+                    }
+                )
+                .fallbackToDestructiveMigration()
+            return db.build()
+        }
+
+        private suspend fun initPrePopulateDefaultGroup(context: Context) {
+            val currentDate = DateTimeProviderImpl().now()
+            val defaultGroup = ToDoGroupDb(
+                id = ToDoGroupDb.DEFAULT_ID,
+                name = "Others",
+                createdAt = currentDate,
+                updatedAt = currentDate
+            )
+            val writeDao = getInstance(context).toDoWriteDao()
+
+            writeDao.insertGroup(listOf(defaultGroup))
+        }
+    }
 }
