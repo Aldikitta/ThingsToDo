87fca12d7c25570ef4e32ee6503ae3a60897e418
Aldik <andi.kitta77@gmail.com>
2022-08-28 18:46:08 +0800

    Local Manager

--- app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/local/LocalManager.kt
+++ app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/local/LocalManager.kt
@@ -1,10 +1,15 @@
 package com.aldikitta.thingstodo.foundation.datasource.local
 
 import com.aldikitta.thingstodo.foundation.di.DiName
+import com.aldikitta.thingstodo.foundation.extension.toDoGroupWithListToGroup
+import com.aldikitta.thingstodo.foundation.extension.toDoListWithTasksToToDoList
+import com.aldikitta.thingstodo.foundation.extension.toToDoGroup
 import com.aldikitta.thingstodo.model.ToDoGroup
+import com.aldikitta.thingstodo.model.ToDoList
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Named
@@ -14,9 +19,32 @@ class LocalManager @Inject constructor(
     private val toDoReadDao: ToDoReadDao,
     private val toDoWriteDao: ToDoWriteDao
 ) {
-    fun getGroup(): Flow<List<ToDoGroup>>{
+    fun getGroup(): Flow<List<ToDoGroup>> {
         return toDoReadDao.getGroup()
             .filterNotNull()
-            .map { it.toTodo }
+            .map { it.toToDoGroup() }
+            .flowOn(dispatcher)
     }
+
+    fun getGroup(groupId: String): Flow<ToDoGroup> {
+        return toDoReadDao.getGroup(groupId)
+            .filterNotNull()
+            .map { it.toToDoGroup() }
+            .flowOn(dispatcher)
+    }
+
+    fun getGroupWithList(): Flow<List<ToDoGroup>>{
+        return toDoReadDao.getGroupWithList()
+            .filterNotNull()
+            .map { it.toDoGroupWithListToGroup() }
+            .flowOn(dispatcher)
+    }
+
+    fun getListWithTasks(): Flow<List<ToDoList>>{
+        return toDoReadDao.getListWithTasks()
+            .filterNotNull()
+            .map { it.toDoListWithTasksToToDoList() }
+            .flowOn(dispatcher)
+    }
+
 }
