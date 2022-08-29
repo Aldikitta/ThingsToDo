a5fcc022299d846af74f42d89589abb3384d2660
Aldik <andi.kitta77@gmail.com>
2022-08-29 13:33:06 +0800

    Local Manager

--- app/src/main/java/com/aldikitta/thingstodo/features/host/ui/HostViewModel.kt
+++ app/src/main/java/com/aldikitta/thingstodo/features/host/ui/HostViewModel.kt
@@ -1,8 +1,9 @@
 package com.aldikitta.thingstodo.features.host.ui
 
+import com.aldikitta.thingstodo.features.host.data.IHostEnvironment
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HostViewModel @Inject constructor(hostEnvironment: ) {
+class HostViewModel @Inject constructor(hostEnvironment: IHostEnvironment) :  {
 }
--- app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/preferences/CredentialPreferenceSerializer.kt
+++ app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/preferences/CredentialPreferenceSerializer.kt
@@ -2,5 +2,5 @@ package com.aldikitta.thingstodo.foundation.datasource.preferences
 
 import androidx.datastore.core.Serializer
 
-object CredentialPreferenceSerializer: Serializer<Cred> {
-}
+//object CredentialPreferenceSerializer: Serializer<Cred> {
+//}
