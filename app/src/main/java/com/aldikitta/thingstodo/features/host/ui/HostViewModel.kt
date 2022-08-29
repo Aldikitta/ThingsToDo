d5f28f0af125cd3cd9cca3f80f810d143003f8f8
Aldik <andi.kitta77@gmail.com>
2022-08-29 13:35:04 +0800

    Local Manager

--- app/src/main/java/com/aldikitta/thingstodo/features/host/ui/HostViewModel.kt
+++ app/src/main/java/com/aldikitta/thingstodo/features/host/ui/HostViewModel.kt
@@ -1,9 +1,28 @@
 package com.aldikitta.thingstodo.features.host.ui
 
+import androidx.lifecycle.viewModelScope
 import com.aldikitta.thingstodo.features.host.data.IHostEnvironment
+import com.aldikitta.thingstodo.foundation.viewmodel.StatefulViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HostViewModel @Inject constructor(hostEnvironment: IHostEnvironment) :  {
+class HostViewModel @Inject constructor(hostEnvironment: IHostEnvironment) :
+    StatefulViewModel<HostState, Unit, Unit, IHostEnvironment>(HostState(), hostEnvironment) {
+    init {
+        initTheme()
+    }
+
+    override fun dispatch(action: Unit) {
+
+    }
+
+    private fun initTheme() {
+        viewModelScope.launch {
+            environment.getTheme()
+                .collect { setState { copy(theme = it) } }
+        }
+    }
+
 }
