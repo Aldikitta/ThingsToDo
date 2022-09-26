a106692746cef7e9dfdf8a0dc27ea34b8041f904
Aldik <andi.kitta77@gmail.com>
2022-09-26 16:11:00 +0800

    Credentials

--- app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/preferences/CredentialPreferenceSerializer.kt
+++ app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/preferences/CredentialPreferenceSerializer.kt
@@ -1,6 +1,13 @@
 package com.aldikitta.thingstodo.foundation.datasource.preferences
 
+import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 
-//object CredentialPreferenceSerializer: Serializer<Cred> {
+//object CredentialPreferenceSerializer : Serializer<> {
+//
+////    override val defaultValue: CredentialPreference = CredentialPreference(token = "")
+//
 //}
