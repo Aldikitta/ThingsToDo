d27233d19198d1e44de358843dfcbeb05d85354e
Aldik <andi.kitta77@gmail.com>
2022-09-26 18:45:28 +0800

    preferences datastore proto

--- app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/preferences/LanguagePreferenceSerializer.kt
+++ app/src/main/java/com/aldikitta/thingstodo/foundation/datasource/preferences/LanguagePreferenceSerializer.kt
@@ -1,27 +1,28 @@
-//package com.aldikitta.thingstodo.foundation.datasource.preferences
-//
-//import androidx.datastore.core.CorruptionException
-//import androidx.datastore.core.Serializer
-//import java.io.IOException
-//import java.io.InputStream
-//import java.io.OutputStream
-//
-//object LanguagePreferenceSerializer : Serializer<LanguagePreference> {
-//
-//    override val defaultValue: LanguagePreference = LanguagePreference.ENGLISH
-//
-//    @Suppress("BlockingMethodInNonBlockingContext")
-//    override suspend fun readFrom(input: InputStream): LanguagePreference {
-//        try {
-//            return LanguagePreference.ADAPTER.decode(input)
-//        } catch (exception: IOException) {
-//            throw CorruptionException("Cannot read proto", exception)
-//        }
-//    }
-//
-//    @Suppress("BlockingMethodInNonBlockingContext")
-//    override suspend fun writeTo(t: LanguagePreference, output: OutputStream) {
-//        LanguagePreference.ADAPTER.encode(output, t)
-//    }
-//
-//}
+package com.aldikitta.thingstodo.foundation.datasource.preferences
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.aldikitta.thingstodo.foundation.datasource.preferences.model.LanguagePreference
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+object LanguagePreferenceSerializer : Serializer<LanguagePreference> {
+
+    override val defaultValue: LanguagePreference = LanguagePreference.ENGLISH
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun readFrom(input: InputStream): LanguagePreference {
+        try {
+            return LanguagePreference.ADAPTER.decode(input)
+        } catch (exception: IOException) {
+            throw CorruptionException("Cannot read proto", exception)
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun writeTo(t: LanguagePreference, output: OutputStream) {
+        LanguagePreference.ADAPTER.encode(output, t)
+    }
+
+}
--- app/src/main/java/com/aldikitta/thingstodo/foundation/di/DataStoreModule.kt
+++ app/src/main/java/com/aldikitta/thingstodo/foundation/di/DataStoreModule.kt
@@ -4,14 +4,19 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
 import com.aldikitta.thingstodo.foundation.datasource.preferences.CredentialPreferenceSerializer
+import com.aldikitta.thingstodo.foundation.datasource.preferences.LanguagePreferenceSerializer
+import com.aldikitta.thingstodo.foundation.datasource.preferences.ThemePreferenceSerializer
+import com.aldikitta.thingstodo.foundation.datasource.preferences.UserPreferenceSerializer
 import com.aldikitta.thingstodo.foundation.datasource.preferences.model.CredentialPreference
+import com.aldikitta.thingstodo.foundation.datasource.preferences.model.LanguagePreference
+import com.aldikitta.thingstodo.foundation.datasource.preferences.model.ThemePreference
+import com.aldikitta.thingstodo.foundation.datasource.preferences.model.UserPreference
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
 
 private const val CREDENTIAL_NAME = "credential-preference.pb"
 private const val USER_NAME = "user-preference.pb"
@@ -22,19 +27,19 @@ private val Context.credentialDataStore: DataStore<CredentialPreference> by data
     fileName = CREDENTIAL_NAME,
     serializer = CredentialPreferenceSerializer
 )
-//private val Context.userDataStore: DataStore<UserPreference> by dataStore(
-//    fileName = USER_NAME,
-//    serializer = UserPreferenceSerializer
-//)
-//private val Context.themeDataStore: DataStore<ThemePreference> by dataStore(
-//    fileName = THEME_NAME,
-//    serializer = ThemePreferenceSerializer
-//)
-//val Context.languageDatastore: DataStore<LanguagePreference> by dataStore(
-//    fileName = LANGUAGE_NAME,
-//    serializer = LanguagePreferenceSerializer
-//)
-//
+private val Context.userDataStore: DataStore<UserPreference> by dataStore(
+    fileName = USER_NAME,
+    serializer = UserPreferenceSerializer
+)
+private val Context.themeDataStore: DataStore<ThemePreference> by dataStore(
+    fileName = THEME_NAME,
+    serializer = ThemePreferenceSerializer
+)
+val Context.languageDatastore: DataStore<LanguagePreference> by dataStore(
+    fileName = LANGUAGE_NAME,
+    serializer = LanguagePreferenceSerializer
+)
+
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
@@ -44,23 +49,23 @@ object DataStoreModule {
     fun provideCredentialDataStore(@ApplicationContext context: Context): DataStore<CredentialPreference> {
         return context.credentialDataStore
     }
-//
-//    @Singleton
-//    @Provides
-//    fun provideUserDataStore(@ApplicationContext context: Context): DataStore<UserPreference> {
-//        return context.userDataStore
-//    }
-//
-//    @Singleton
-//    @Provides
-//    fun provideThemeDataStore(@ApplicationContext context: Context): DataStore<ThemePreference> {
-//        return context.themeDataStore
-//    }
-//
-//    @Singleton
-//    @Provides
-//    fun provideLanguageDataStore(@ApplicationContext context: Context): DataStore<LanguagePreference> {
-//        return context.languageDatastore
-//    }
-//
+
+    @Singleton
+    @Provides
+    fun provideUserDataStore(@ApplicationContext context: Context): DataStore<UserPreference> {
+        return context.userDataStore
+    }
+
+    @Singleton
+    @Provides
+    fun provideThemeDataStore(@ApplicationContext context: Context): DataStore<ThemePreference> {
+        return context.themeDataStore
+    }
+
+    @Singleton
+    @Provides
+    fun provideLanguageDataStore(@ApplicationContext context: Context): DataStore<LanguagePreference> {
+        return context.languageDatastore
+    }
+
 }
